67776a80a3d4aa1a9a4b8a0165393e1ec5add161
1160301012 <m18846086305@163.com>
2018-03-11 22:17:20 +0800

    1

---
+++ coins/src/coins/Coin.java
@@ -0,0 +1,24 @@
+package coins;
+import java.util.*;
+public class Coin {
+	private HashMap<Integer,String> coinnames=new HashMap<Integer,String>();
+	
+	public Coin() {
+		coinnames.put(1, "penny");
+		coinnames.put(10, "dime");
+		coinnames.put(25, "quater");
+		coinnames.put(50, "half-dollar");		
+	}
+	public String getName(int amount)
+	{
+		return coinnames.get(amount);
+	}
+	
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int amount=in.nextInt();
+		Coin coin=new Coin();
+		String name=coin.getName(amount);
+		System.out.println(name);
+	}
+}
